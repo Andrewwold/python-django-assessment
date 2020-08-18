57a55c434e63e03f3dc6d3a158e28b0645a4aac9
Andrew <goldsabre@gmail.com>
2020-08-18 01:40:47 -0600

    Reseting to working state ' '

--- config/settings/common.py
+++ config/settings/common.py
@@ -108,15 +108,11 @@ TEMPLATES = [
     },
 ]
 
-
-
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(ROOT_DIR, 'staticfiles/')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(APPS_DIR, 'static/'),
 )
-
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
